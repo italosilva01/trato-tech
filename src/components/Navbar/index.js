d106c02cecd60f4f41496ba78c31eb2833a5de14
Francisco Italo <italo.silvan87@gmail.com>
2024-05-17 20:53:11 -0300

    add class "2.2 - Criando a busca"

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,16 +1,13 @@
-import styles from './Navbar.module.scss';
-import { ReactComponent as Logo } from '../../assets/logo.svg';
-import classNames from 'classnames';
-import {
-  RiShoppingCart2Line,
-  RiShoppingCartFill
-} from 'react-icons/ri';
-import Busca from '../Busca';
+import styles from "./Navbar.module.scss";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+import classNames from "classnames";
+import { RiShoppingCart2Line, RiShoppingCartFill } from "react-icons/ri";
+import Search from "components/Search/Search";
 
 const iconeProps = {
-  color: 'white',
-  size: 24
-}
+  color: "white",
+  size: 24,
+};
 
 export default function Navbar() {
   return (
@@ -18,24 +15,28 @@ export default function Navbar() {
       <Logo className={styles.logo} />
       <div className={styles.links}>
         <div>
-          <a href='/' className={classNames(styles.link, {
-            [styles.selected]: window.location.pathname === '/'
-          })}>
+          <a
+            href="/"
+            className={classNames(styles.link, {
+              [styles.selected]: window.location.pathname === "/",
+            })}
+          >
             Página inicial
           </a>
         </div>
       </div>
-      <div className={styles.busca}>
-        <Busca />
+      <div className={styles.search}>
+        <Search />
       </div>
       <div className={styles.icones}>
         <a href="/carrinho">
-          {window.location.pathname === '/carrinho'
-            ? <RiShoppingCartFill {...iconeProps} />
-            : <RiShoppingCart2Line {...iconeProps} />
-          }
+          {window.location.pathname === "/carrinho" ? (
+            <RiShoppingCartFill {...iconeProps} />
+          ) : (
+            <RiShoppingCart2Line {...iconeProps} />
+          )}
         </a>
       </div>
     </nav>
-  )
+  );
 }
---
+++ src/components/Search/Search.js
@@ -0,0 +1,8 @@
+import styles from "./Search.module.scss";
+export default function Search() {
+  return (
+    <div className={styles.search}>
+      <input className={styles.input} placeholder="O que você procura?" />
+    </div>
+  );
+}
